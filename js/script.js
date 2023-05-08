b232de51ceb57a03a8f3a5704aecfdcc0e150303
unknown <luizfelipemlds65@gmail.com>
2023-05-08 20:44:50 -0300

    com js

--- js/script.js
+++ js/script.js
@@ -17,15 +17,13 @@ const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro);
-  console.log(elemento);
+
   if (elemento) {
     elemento.checked = true;
   }
 }
 
 parametros.forEach(ativarProduto);
-
-console.log(parametros);
 
 //prguntas frequentes
 
@@ -39,7 +37,6 @@ function ativarPergunta(event) {
   resposta.classList.toggle("ativa");
   const ativa = resposta.classList.contains(ativa);
   pergunta.setAttribute("aria-expanded", "true");
-  console.log(ativa);
 }
 
 function eventosPerguntas(pergunta) {
@@ -47,3 +44,29 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//galeria de bicicletaa
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+console.log(galeria, galeriaContainer);
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+  console.log(img);
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//animaco
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
